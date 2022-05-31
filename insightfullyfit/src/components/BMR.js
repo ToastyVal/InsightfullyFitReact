f15f1bfe3793d560120255e575251a7afe04d0ac
Malik DePasse <malikdepasse5@gmail.com>
2022-05-31 16:35:35 -0700

    Update BMR.js
    changed to make male calc work

--- insightfullyfit/src/components/BMR.js
+++ insightfullyfit/src/components/BMR.js
@@ -3,33 +3,33 @@ import React from "react";
 function BMR() {
   function femaleMifflin() {
     const age = document.getElementById("age").value;
-    const weight = document.getElementById("weight").value;
-    const height = document.getElementById("height").value;
+    const weight = document.getElementById("weight").value / 2.205;
+    const height = document.getElementById("height").value * 2.54;
     const bmr = 10 * weight + 6.25 * height - 5 * age + 5;
     document.getElementById("mResultsF").innerHTML = bmr;
   }
 
   function femaleHarris() {
     const age = document.getElementById("age").value;
-    const weight = document.getElementById("weight").value;
-    const height = document.getElementById("height").value;
+    const weight = document.getElementById("weight").value / 2.205;
+    const height = document.getElementById("height").value * 2.54;
     const bmr = 13.397 * weight + 4.799 * height - 5.677 * age + 88.362;
     document.getElementById("hResultsF").innerHTML = bmr;
   }
 
   function maleMifflin() {
-    const age = document.getElementById("age").value;
-    const weight = document.getElementById("weight").value;
-    const height = document.getElementById("height").value;
-    const bmr = 10 * weight + 6.25 * height - 5.677 * age - 161;
+    const age = document.getElementById("maleage").value;
+    const weight = document.getElementById("maleweight").value / 2.205;
+    const height = document.getElementById("maleheight").value * 2.54;
+    const bmr = (10 * weight) + (6.25 * height) - (5.677 * age) - 161;
     document.getElementById("mResultsM").innerHTML = bmr;
   }
 
   function maleHarris() {
-    const age = document.getElementById("age").value;
-    const weight = document.getElementById("weight").value;
-    const height = document.getElementById("height").value;
-    const bmr = 9.247 * weight + 3.098 * height - 4.33 * age + 447.593;
+    const age = document.getElementById("maleage").value;
+    const weight = document.getElementById("maleweight").value / 2.205;
+    const height = document.getElementById("maleheight").value * 2.54;
+    const bmr = (9.247 * weight) + (3.098 * height) - (4.33 * age) + 447.593;
     document.getElementById("hResultsM").innerHTML = bmr;
   }
 
@@ -40,9 +40,9 @@ function BMR() {
         <h1 id="FemaleFormula">Female</h1>
         <input type="text" id="age" placeholder="Enter Age" />
         <br />
-        <input type="text" id="weight" placeholder="Enter Weight" />
+        <input type="text" id="weight" placeholder="Enter Weight in lbs." />
         <br />
-        <input type="text" id="height" placeholder="Enter height" />
+        <input type="text" id="height" placeholder="Enter height in inches" />
         <br />
 
         <h1>
@@ -59,11 +59,11 @@ function BMR() {
         </button>
         <div class="male">
           <h1 id="MaleFormula">Male</h1>
-          <input type="text" id="age" placeholder="Enter Age" />
+          <input type="text" id="maleage" placeholder="Enter Age" />
           <br />
-          <input type="text" id="weight" placeholder="Enter Weight" />
+          <input type="text" id="maleweight" placeholder="Enter Weight in lbs." />
           <br />
-          <input type="text" id="height" placeholder="Enter height" />
+          <input type="text" id="maleheight" placeholder="Enter height in inches" />
           <br />
 
           <h1>
